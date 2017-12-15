c5ddf28a47f48641a2d92a1036c81838389f44b6
Brayden Crabb <brayden.crabb@warriorlife.net>
2017-12-15 10:52:14 -0800

    Added parmesean to the background

--- calculator.js
+++ calculator.js
@@ -1,1 +1,24 @@
 // JavaScript Document
+
+// Change Display
+function d(val) {
+	"use strict";
+	document.getElementById("d").value = val;
+}
+
+// Type nmbers and operators
+function v(val) {
+	"use strict";
+	document.getElementById("d").value += val;
+}
+
+// Evaluate the equation
+function e() {
+	"use strict";
+	try {
+		d(eval(document.getElementById("d").value));
+	}
+	catch(err) {
+		d("Error");
+	}
+}
